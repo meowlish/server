36b74b392a01cf98ad416384f5171a119edf36aa
D-ng1277 <haidangbachkhoa@gmail.com>
2026-03-06 20:12:32 +0700

    chore: rename

--- apps/exam/src/app/commands/handlers/index.ts
+++ apps/exam/src/app/commands/handlers/index.ts
@@ -1,21 +1,21 @@
+import { CreateExamHandler } from './management/create-exam.handler';
+import { CreateQuestionHandler } from './management/create-question.handler';
+import { CreateSectionHandler } from './management/create-section.handler';
+import { DeleteExamHandler } from './management/delete-exam.handler';
+import { DeleteQuestionHandler } from './management/delete-question.handler';
+import { DeleteSectionHandler } from './management/delete-section.handler';
+import { MoveQuestionHandler } from './management/move-question.handler';
+import { MoveSectionHandler } from './management/move-section.handler';
+import { ReviewExamHandler } from './management/review-exam.handler';
+import { UpdateExamHandler } from './management/update-exam.handler';
+import { UpdateQuestionHandler } from './management/update-question.handler';
+import { UpdateSectionHandler } from './management/update-section.handler';
 import { AddNoteHandler } from './practice/add-note.handler';
 import { AnswerHandler } from './practice/answer.handler';
 import { AttemptHandler } from './practice/attempt.handler';
 import { EndAttemptHandler } from './practice/end-attempt.handler';
 import { RemoveAnswerHandler } from './practice/remove-answer.handler';
 import { ToggleFlagHandler } from './practice/toggle-flag.handler';
-import { CreateExamHandler } from './staff/create-exam.handler';
-import { CreateQuestionHandler } from './staff/create-question.handler';
-import { CreateSectionHandler } from './staff/create-section.handler';
-import { DeleteExamHandler } from './staff/delete-exam.handler';
-import { DeleteQuestionHandler } from './staff/delete-question.handler';
-import { DeleteSectionHandler } from './staff/delete-section.handler';
-import { MoveQuestionHandler } from './staff/move-question.handler';
-import { MoveSectionHandler } from './staff/move-section.handler';
-import { ReviewExamHandler } from './staff/review-exam.handler';
-import { UpdateExamHandler } from './staff/update-exam.handler';
-import { UpdateQuestionHandler } from './staff/update-question.handler';
-import { UpdateSectionHandler } from './staff/update-section.handler';
 
 export const ExamManagementHandlers = [
 	CreateExamHandler,
