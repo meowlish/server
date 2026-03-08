55c329d6c8b431567e772172a903643909bd7b7f
Linh Mã <117853392+linhma139@users.noreply.github.com>
2026-03-09 00:19:38 +0700

    fix: fix lint error

--- apps/content/src/app/blog/blog.module.ts
+++ apps/content/src/app/blog/blog.module.ts
@@ -2,7 +2,7 @@ import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaClient } from '@prisma-client/blog';
+import { PrismaClient } from '@prisma-client/content';
 import { DatabaseModule } from '@server/database';
 import { LoggerModule } from '@server/logger';
 
