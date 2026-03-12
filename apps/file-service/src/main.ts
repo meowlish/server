cc8c3ab2f120cb2a999c41db677a7cf15c2947b0
D-ng1277 <haidangbachkhoa@gmail.com>
2026-03-12 11:00:58 +0700

    chore: module init for file service

---
+++ apps/file-service/src/main.ts
@@ -0,0 +1,26 @@
+import { FileModule } from './app/app.module';
+import { INestApplicationContext } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppLoggerService } from '@server/logger';
+import { join } from 'path';
+
+const useLogger = (module: INestApplicationContext) => {
+	const logger = module.get(AppLoggerService);
+	module.useLogger(logger);
+};
+
+async function bootstrap() {
+	const examModule = await NestFactory.createMicroservice<MicroserviceOptions>(FileModule, {
+		transport: Transport.GRPC,
+		options: {
+			url: `${process.env.HOST ?? '127.0.0.1'}:${process.env.PORT ?? 50052}`,
+			package: 'exam',
+			protoPath: join(process.cwd(), 'proto', 'exam.proto'),
+		},
+	});
+	useLogger(examModule);
+	await examModule.listen();
+}
+
+bootstrap().catch(console.error);
