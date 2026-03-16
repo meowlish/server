091a5aa590387b4a97a00f45a6800b447b4d2d9f
D-ng1277 <haidangbachkhoa@gmail.com>
2026-03-16 23:28:45 +0700

    chore: file and achievement service config

--- apps/achievement/src/achievement.module.ts
+++ apps/achievement/src/achievement.module.ts
@@ -1,7 +1,61 @@
+import { config } from './configs/config';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD, APP_PIPE } from '@nestjs/core';
+import { CqrsModule } from '@nestjs/cqrs';
+import { PrismaClient } from '@prisma-client/auth';
+import { DATABASE_SERVICE, DatabaseModule } from '@server/database';
 import { LoggerModule } from '@server/logger';
+import {
+	Any2RpcExceptionFilter,
+	GlobalValidationPipe,
+	Http2gRPCExceptionFilter,
+} from '@server/utils';
+import { ClsGuard, ClsModule } from 'nestjs-cls';
 
 @Module({
-	imports: [LoggerModule.forRoot({ appName: 'FileModule' })],
+	imports: [
+		ConfigModule.forRoot({
+			expandVariables: true,
+			cache: true,
+			isGlobal: true,
+			load: [config],
+		}),
+		CqrsModule.forRoot(),
+		ClsModule.forRoot({
+			global: true,
+			guard: { mount: false },
+			plugins: [
+				new ClsPluginTransactional({
+					imports: [DatabaseModule.forRoot(PrismaClient)],
+					adapter: new TransactionalAdapterPrisma({
+						prismaInjectionToken: DATABASE_SERVICE,
+						sqlFlavor: 'postgresql',
+					}),
+				}),
+			],
+		}),
+		LoggerModule.forRoot({ appName: 'AchievementModule' }),
+	],
+	providers: [
+		{
+			provide: APP_GUARD,
+			useClass: ClsGuard,
+		},
+		{
+			provide: APP_FILTER,
+			useClass: Any2RpcExceptionFilter,
+		},
+		{
+			provide: APP_FILTER,
+			useClass: Http2gRPCExceptionFilter,
+		},
+		{
+			provide: APP_PIPE,
+			useClass: GlobalValidationPipe,
+		},
+	],
 })
 export class AchievementModule {}
