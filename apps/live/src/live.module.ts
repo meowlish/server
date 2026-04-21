aa29a8098605ebabf4780dec93a302bc389e5327
D-ng1277 <haidangbachkhoa@gmail.com>
2026-04-21 22:16:09 +0700

    chore: some more nest boilerplate for `live`

---
+++ apps/live/src/live.module.ts
@@ -0,0 +1,72 @@
+import { bullConfig } from './configs/bullmq.config';
+import { config } from './configs/config';
+import { rmqSubConfig } from './configs/rmq.sub.config';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { BullModule } from '@nestjs/bullmq';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { PrismaClient } from '@prisma-client/live';
+import { DATABASE_SERVICE, DatabaseModule } from '@server/database';
+import { LoggerModule } from '@server/logger';
+import {
+	Any2RpcExceptionFilter,
+	GlobalClassSerializerInterceptor,
+	GlobalValidationPipe,
+	Http2gRPCExceptionFilter,
+} from '@server/utils';
+import { ClsGuard, ClsModule } from 'nestjs-cls';
+
+@Module({
+	controllers: [],
+	imports: [
+		ConfigModule.forRoot({
+			expandVariables: true,
+			cache: true,
+			isGlobal: true,
+			load: [config],
+		}),
+		ClsModule.forRoot({
+			global: true,
+			guard: { mount: false },
+			plugins: [
+				new ClsPluginTransactional({
+					imports: [DatabaseModule.forRoot(PrismaClient)],
+					adapter: new TransactionalAdapterPrisma({
+						prismaInjectionToken: DATABASE_SERVICE,
+						sqlFlavor: 'postgresql',
+					}),
+				}),
+			],
+		}),
+		BullModule.forRootAsync({ inject: [ConfigService], useFactory: bullConfig }),
+		BullModule.registerQueue({ name: 'live' }),
+		RabbitMQModule.forRootAsync({ inject: [ConfigService], useFactory: rmqSubConfig }),
+		LoggerModule.forRoot({ appName: 'LiveModule' }),
+	],
+	providers: [
+		{
+			provide: APP_GUARD,
+			useClass: ClsGuard,
+		},
+		{
+			provide: APP_FILTER,
+			useClass: Any2RpcExceptionFilter,
+		},
+		{
+			provide: APP_FILTER,
+			useClass: Http2gRPCExceptionFilter,
+		},
+		{
+			provide: APP_PIPE,
+			useClass: GlobalValidationPipe,
+		},
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: GlobalClassSerializerInterceptor,
+		},
+	],
+})
+export class LiveModule {}
--- apps/live/src/main.ts
+++ apps/live/src/main.ts
@@ -1,18 +1,30 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-import { AppModule } from './app/app.module';
-import { Logger } from '@nestjs/common';
+import { LiveModule } from './live.module';
+import { PackageDefinition } from '@grpc/grpc-js/build/src/make-client';
+import { INestApplicationContext } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { live } from '@server/generated';
+import { AppLoggerService } from '@server/logger';
+import 'reflect-metadata';
+
+const useLogger = (module: INestApplicationContext) => {
+	const logger = module.get(AppLoggerService);
+	module.useLogger(logger);
+};
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	const globalPrefix = 'api';
-	app.setGlobalPrefix(globalPrefix);
-	const port = process.env.PORT || 3000;
-	await app.listen(port);
-	Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+	const liveModule = await NestFactory.createMicroservice<MicroserviceOptions>(LiveModule, {
+		transport: Transport.GRPC,
+		options: {
+			url: `${process.env.HOST ?? '127.0.0.1'}:${process.env.PORT ?? 50050}`,
+			package: 'live',
+			packageDefinition: {
+				[`live.${live.LIVE_SERVICE_NAME}`]: live.LiveServiceService,
+			} satisfies PackageDefinition,
+		},
+	});
+	useLogger(liveModule);
+	await liveModule.listen();
 }
 
-bootstrap();
+bootstrap().catch(console.error);
