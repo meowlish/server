7b67e8603eb5669a5bf5c1cc6ef0b9ce8a1ca18e
D-ng1277 <haidangbachkhoa@gmail.com>
2026-04-21 20:40:27 +0700

    chore: init `live` service

---
+++ apps/live/eslint.config.mjs
@@ -0,0 +1,24 @@
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+	...baseConfig,
+	{
+		files: [
+			'**/*.ts',
+			'**/*.tsx',
+			'**/*.cts',
+			'**/*.mts',
+			'**/*.js',
+			'**/*.jsx',
+			'**/*.cjs',
+			'**/*.mjs',
+		],
+		ignores: ['**/*.config.{js,ts,mjs,cjs,cts}', '**/generated/**', '**/dist', '**/out-tsc'],
+		languageOptions: {
+			parserOptions: {
+				projectService: false,
+				project: ['apps/live/tsconfig.*?.json'],
+			},
+		},
+	},
+];
